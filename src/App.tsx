863fedbe1385f7d26c5c537a4a2ce0b74661de86
dxmlk <1101sky@korea.ac.kr>
2026-03-20 18:05:50 +0900

    feat: 안드로이드 사전신청 리다이렉트

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// App.tsx
 import { useEffect } from "react";
 import "./App.css";
 import { Mobile, PC } from "./components/Responsive";
@@ -6,7 +5,9 @@ import MobilePage from "./components/tempPages/MobilePage";
 import PCPage from "./components/tempPages/PCPage";
 
 const APP_STORE_URL =
-  "https://apps.apple.com/kr/app/%EC%83%88%EB%A1%9D-%EC%9D%BC%EC%83%81-%EC%86%8D%EC%9D%98-%ED%83%90%EC%A1%B0-%EC%9D%BC%EC%A7%80/id6744866662"; // ← 여기에 실제 앱스토어 주소 넣으세요
+  "https://apps.apple.com/kr/app/%EC%83%88%EB%A1%9D-%EC%9D%BC%EC%83%81-%EC%86%8D%EC%9D%98-%ED%83%90%EC%A1%B0-%EC%9D%BC%EC%A7%80/id6744866662";
+const ANDROID_PRE_REGISTRATION_URL =
+  "https://docs.google.com/forms/d/e/1FAIpQLSc_LJXcdnmmNC4smdBlBnR_G8z9XvhY_1oyXEkVyyoSFOLF5Q/viewform?usp=dialog";
 
 function App() {
   useEffect(() => {
@@ -17,14 +18,17 @@ function App() {
       /iPhone|iPad|iPod/i.test(ua) ||
       // iPadOS 13+가 desktop Safari처럼 나오는 경우 처리
       (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
+    const isAndroid = /Android/i.test(ua);
 
     if (isIOS) {
       window.location.href = APP_STORE_URL;
+      return;
+    }
+
+    if (isAndroid) {
+      window.location.href = ANDROID_PRE_REGISTRATION_URL;
     }
   }, []);
-
-  // const router = createRouter();
-  // return <RouterProvider router={router} />;
 
   return (
     <>
